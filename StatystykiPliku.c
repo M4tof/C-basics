53550d5c0414603594ac92c40287788215c0d2c2
Krzysztof Mańczak <krzysztfmanczak@gmail.com>
2023-04-07 11:31:12 +0200

    works

--- StatystykiPliku.c
+++ StatystykiPliku.c
@@ -5,41 +5,41 @@
 
 
 void Count_Numbers(FILE* origianl,FILE* new_file){
-    int Suma[1000]={0};
-    int Licznosc[1000]={0};
-    int final=0;
     char line[1000];
-    char *iteam;
-    int numer;
+    double Suma[1000]={0};
+    int Licznosc[1000]={0};
+    int maxim=0;
 
     while(fgets(line,100,origianl)!=NULL){
-        int current=0;
-        numer=0;
-        iteam = strtok(line, " \n");
+        int current =0;
+        int position =0;
+        char* iteam = strtok(line," ");
+        
         while (iteam != NULL){
-            Suma[numer]+=atoi(iteam);
-            Licznosc[numer]++;
-            current++;
-            iteam = strtok(NULL, " \n");
+            current = atoi(iteam);
+            
+            Suma[position]+=current;
+            Licznosc[position]++;
+            position++;
+            
+            iteam = strtok(NULL, " ");
         }
-        if (current > final){
-            final=current;
+        if (position>maxim){
+            maxim=position;
         }
     }
-    //TODO//
-    
-
-    fprintf(new_file,"Suma:");
-    for(int i=0;i<=final;i++){
-        fprintf(new_file," %d",Suma[i]);
+    fprintf(new_file,"SUMA:");
+    for(int i=0;i<maxim;i++){
+        int A_int;
+        A_int = (int) Suma[i];
+        fprintf(new_file," %d",A_int);
     }
     fprintf(new_file,"\n");
 
-    fprintf(new_file,"Srednia:");
-    for(int g=0;g<=final;g++){
-        fprintf(new_file," %d",(Suma[g]/Licznosc[g]));
+    fprintf(new_file,"SREDNIA:");
+    for(int g=0;g<maxim;g++){
+        fprintf(new_file," %.2lf",(Suma[g]/Licznosc[g]));
     }
-
 }
 
 
@@ -71,4 +71,4 @@ int main(){
     fclose(new_file);   
 
     return 0;   
-}   
+}
