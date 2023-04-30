f1f1e5cd63f54d98a2635bc86cee0ad81ac7ad5f
Krzysztof Mańczak <krzysztfmanczak@gmail.com>
2023-04-30 12:56:50 +0200

    more

--- MacierzRownosciPrzewagi.c
+++ MacierzRownosciPrzewagi.c
@@ -2,6 +2,28 @@
 #include <stdlib.h>
 #include <math.h>
 
+void op1(int n,int m, double Ma[n][m],double Mb[n][m],double Mr[n][m]){
+    for(int y=0;y<n;y++){
+        for(int x=0;x<m;x++){
+            Mr[y][x]=0;
+            if(Ma[y][x]==Mb[y][x]){
+                Mr[y][x]=1;
+            }
+        }
+    }
+}
+
+void op2(int n,int m, double Ma[n][m],double Mb[n][m],double Mp[n][m]){
+    for(int y=0;y<n;y++){
+        for(int x=0;x<m;x++){
+            Mp[y][x]=0;
+            if(Ma[y][x] > Mb[y][x]){
+                Mp[y][x]=1;
+            }
+        }
+    }
+}
+
 void main(){ 
     int n;
     int m;
@@ -34,11 +56,43 @@ void main(){
         }
 
     }
+    
     double A[n][m];
     double B[n][m];
     double R[n][m];
     double P[n][m];
     
+    for(int y=0;y<n;y++){
+        printf("Podaj dane do wiersza %d macierzy A: ",y);
+        for(int x=0;x<m;x++){
+            scanf(" %lf",&A[y][x]);
+        }
+    }
+    
+    for(int y=0;y<n;y++){
+        printf("Podaj dane do wiersza %d macierzy B: ",y);
+        for(int x=0;x<m;x++){
+            scanf(" %lf",&B[y][x]);
+        }
+    }
+
+    op1(n,m,A,B,R);
+    op2(n,m,A,B,P);
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            printf("%lf ",R[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            printf("%lf ",P[i][j]);
+        }
+        printf("\n");
+    }
 
 
 }
--- test.c
+++ test.c
@@ -2,21 +2,16 @@
 #include <stdlib.h>
 #include <math.h>
 
-void main(){ 
-    int n;
-    int m;
-    int test =0;
-    printf("Podaj dane do macierzy: \n");
-    
-    while (test=0)
-    {        
-        printf("Wartosc n:");
-        scanf(" %d",&n);
-        
-        test=1;
-    }
-    
+void op(int m, int n, double A[n][m]){
+    A[0][0] = 9;
+}
 
+int main(){ 
     
-
+    double A[3][3]= {{1,2,3},{4,5,6},{7,8,9}};
+    
+    op(3,3,A);
+    
+    printf("%lf",A[0][0]);
+    return 0;
 }
